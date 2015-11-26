cdbe0b889f134b7b7eb38d27f898bbf050e4826e
julian7716 <juliant7@hotmail.com>
2015-11-26 18:30:09 -0500

    se agrega avion_controller, insertar_avion_controller y gestions_aviones

---
+++ gestion_aviones.php
@@ -0,0 +1,47 @@
+
+<?php ?>
+<html>
+    <head>
+        <?php
+        include ('./templates/importCss.php');
+        include('templates/headerAdmin.php');
+        include ('controller/ciudad_controller.php');
+        include('templates/tabla.php');
+        ?>
+    </head>
+    <body>  
+        <div class="container">
+            <div class="wrapper">
+                <div class="panel-heading"><h2>Gesti&oacute;n de aviones</h2></div>
+                <div class="panel panel-warning" >
+                    <div class="panel-body">
+                        <form class="form-signin" name="nuevoAvion" id="vuelos" method = "POST" action = controller/insertar_avion_controller.php>    
+                            <div class="col-sm-4">
+                                <label>TIPO AVION</label><br>
+                                <select class="form-control" name="paises">
+                                    <option>Seleccione ... </option>
+                                    <?php foreach ($aviones as $avion) { ?>
+                                        <option name= '<?php echo $avion[0]; ?>'> <?php echo $avion[1]; ?> </option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="col-sm-4">
+                                <label>FECHA DE INGRESO</label><br>
+                                <input type="date" class="form-control" id="fecha_salida" name="fecha_ingreso" required=""/>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button type="submit" class="btn btn-xl">Nueva Ciudad</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <?php
+        include './templates/importJS.php';
+        ?>
+    </body>
+</html>
