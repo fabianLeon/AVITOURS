c31796efabf58f799606357ccc4ce68d79ee2eea
Desktop <Desktop@Desktop-PC>
2015-11-26 12:46:58 -0500

    ciudad

--- ciudad.php
+++ ciudad.php
@@ -4,14 +4,39 @@
         <?php
         include ('./templates/importCss.php');
         include('templates/headerAdmin.php');
-        include ('controller/vuelo_controller.php');
+        include ('controller/ciudad_controller.php');
         include('templates/tabla.php');
         ?>
     </head>
     <body>  
         <div class="container">
             <div class="wrapper">
-                
+                <div class="panel-heading"><h2>Gesti&oacute;n de Ciudades</h2></div>
+                <div class="panel panel-warning" >
+                    <div class="panel-body">
+                        <form class="form-signin" name="vuelos" id="vuelos" method = "POST" action = home.php>    
+                            <div class="col-sm-4">
+                                <label>PAIS</label><br>
+                                <select class="form-control" id="pais[]">
+                                    <option>Seleccione ... </option>
+                                    <?php foreach ($pais as $paises) { ?>
+                                        <option id= '<?php echo $pais . id ?>'> <?php echo $pais . value ?> ></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="col-sm-4">
+                                <label>CIUDAD</label><br>
+                                <input type="text" class="form-control" name="ciudad" required="" placeholder="Ingrese Ciudad *"/>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button type="submit" class="btn btn-xl">Nueva Ciudad</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div> 
         <?php
---
+++ controller/insertar_ciudad_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+include '../conf.php';
+include '../daos/dao.php';
+include '../daos/daoGestion.php';
+session_start();
+$dao = new dao(DB_HOST, $_SESSION['db_user'], $_SESSION['db_pass'], DB_NAME);
+$dao->conectar();
+$daoGestion = new daoGestion($dao);
+
+if ($_POST){
+    //to do ...
+}
+
