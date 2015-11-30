c89b2d39c422c3d2e322434f899ed41d2a12fe5d
Desktop <Desktop@192.168.10.104>
2015-11-30 17:45:48 -0500

    reserva

--- gestion_vuelo.php
+++ gestion_vuelo.php
@@ -7,6 +7,55 @@
         include ('controller/vuelos_controller.php');
         include('templates/tabla.php');
         ?>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        <style type="text/css">
+            html { height: 100% }
+            body { height: 100%; margin: 0; padding: 0 }
+            #map_canvas { height: 100% }
+        </style>
+        <script type="text/javascript"
+                src="http://maps.googleapis.com/maps/api/js?sensor=false">
+        </script>
+        <script type="text/javascript">
+
+            function initialize() {
+                var myOptions = {
+                    center: new google.maps.LatLng(40.036026, -4.965820),
+                    zoom: 1,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+                };
+
+                var map = new google.maps.Map(document.getElementById("map_canvas"),
+                        myOptions);
+
+                gozilla3 = new google.maps.Marker({
+                    position: new google.maps.LatLng(15.0203, 102.09),
+                    icon: 'http://www.google.com/mapfiles/marker.png',
+                    map: map,
+                    title: 'avion'
+                });
+                gozilla = new google.maps.Marker({
+                    position: new google.maps.LatLng(3.455371, -76.536664),
+                    icon: 'http://www.google.com/mapfiles/marker.png',
+                    map: map,
+                    title: 'avion1'
+                });
+                var ruta = [
+                    new google.maps.LatLng(3.455371, -76.536664),
+                    new google.maps.LatLng(15.0203, 102.09)
+                ];
+
+                var lineas = new google.maps.Polyline({
+                    path: ruta,
+                    map: map,
+                    strokeColor: '#000000',
+                    strokeWeight: 4,
+                    strokeOpacity: 0.8,
+                    clickable: false
+                });
+            }
+        </script>
     </head>
     <body>  
         <div class="container">
@@ -110,16 +159,39 @@
             </div>
         </div>
         <br>
+        <div class="container">
+            <button type="button" class="btn btn-primary" onclick="initialize()" data-toggle="modal" data-target="#modal-1">Activate the button</button>
+
+            <div class="modal fade" id="modal-1">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" >&times;</button>
+                            <h3 class="modal-title">This is the heading</h3>
+                        </div>
+                        <div class="modal-body">
+                            <div id="map_canvas" style="width:100%; height:60%"></div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <a href="" class="btn btn-default" data-dismiss="modal">Close</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <?php
         $titulos = ["ID", "Origen", "Destino", "Fecha Salida", "Fecha LLegada", "Duracion",];
         $tabla = new Tabla($titulos, $tablaVuelosGuardados);
         $tabla->escribirRegistros2();
         echo ($tabla->getTabla());
         ?>
-    </div>
 
-    <?php
-    include './templates/importJS.php';
-    ?>
-</body>
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Plugin JavaScript -->
+
+    </body>
 </html>
